f895c700cb1fa444cbcfbdb889aca0a516bf3670
alesikivan <obcofficiall@gmail.com>
2022-06-03 11:05:38 +0300

    feature(app): add eco config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: './index.js',
+    env : {
+      'PORT': 80,
+      'NODE_ENV' : 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : ['45.93.136.98'],
+      ref  : 'origin/main',
+      repo : 'https://github.com/alesikivan/core-bookmark-red',
+      path : '/root/apps/bookmark-core',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
