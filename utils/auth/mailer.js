7448157b7d3f1c26b2e317735d2cbcea7626c760
alesikivan <alesik.ivan.job@gmail.com>
2023-01-29 19:54:46 +0300

    fix (mailer): change sucure option

--- utils/auth/mailer.js
+++ utils/auth/mailer.js
@@ -10,7 +10,7 @@ const sendEmail = async (options) => {
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
